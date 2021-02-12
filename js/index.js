581f9c11b795e21dae15108836623d484e0ccc7a
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2021-02-11 19:34:06 -0800

    I added two event listemers. One is a mouseover that changes the color of the header to orange. The second is a clcik that turns the header green.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let header = document.querySelector('.main-navigation')
+let nav = document.querySelector('.nav')
+
+header.addEventListener('mouseover', event => {
+    header.style.backgroundColor = 'orange'
+})
+header.addEventListener('click', event => {
+    header.style.backgroundColor = 'green'
+})
+nav.addEventListener('keydown', event => {
+    nav.style.backgroundColor = 'blue'
+})
